403f49ef71752f3edffac2d1dafc32fdbd0551b2
iChris <christianjoseph473@gmail.com>
2023-06-25 02:25:19 +0100

    wrapping up desktop

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
+AOS.init();
+
 const contextText = document.querySelector('.contextText');
 
 document.querySelector('.disclaimerSVG').addEventListener('click', () => {
-    if (contextText.style.display === 'none') {
-        contextText.style.display = ''; // Set the default value
-        contextText.scrollIntoView({ behavior: 'smooth' });
-    } else {
-        contextText.style.display = 'none'; // Set the 'none' value
-    }
+  if (contextText.style.display === 'none') {
+    contextText.style.display = ''; // Set the default value
+    contextText.scrollIntoView({ behavior: 'smooth' });
+  } else {
+    contextText.style.display = 'none'; // Set the 'none' value
+  }
 });
 
-const copyBtn = document.querySelector('.copySVG');
-// copyBtn.addEventListener('click', copyToClipboard);
+const copyBtn = document.querySelector('.copyBTN');
+copyBtn.addEventListener('click', copyToClipboard);
 
 function copyToClipboard() {
-    const textToCopy = document.querySelector('.contractAdd')
-    navigator.clipboard.writeText(textToCopy.innerText)
-        .then(() => {
-            copyBtn.style.stroke = '#02a701';
-            textToCopy.style.color = "green"
-            setTimeout(() => {
-                copyBtn.style.stroke = '';
-                textToCopy.style.color = ""
-            }, 2000)
-        })
-        .catch((error) => {
-            window.alert('Unable to copy text to clipboard');
-        });
+  const textToCopy = document.querySelector('.contractAdd')
+  navigator.clipboard.writeText(textToCopy.innerText)
+    .then(() => {
+      textToCopy.style.fontFamily = "JabHeavy"
+      setTimeout(() => {
+        textToCopy.style.fontFamily = "JabLight"
+      }, 2000)
+    })
+    .catch((error) => {
+      window.alert('Unable to copy text to clipboard');
+    });
 };
+
 
 // LANDING PAGE ANIMATION
 var scene = document.getElementById("scene");

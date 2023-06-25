db7ac559039d3a84a8aa8e83fb8369e9ca574c49
iChris <christianjoseph473@gmail.com>
2023-06-25 19:46:06 +0100

    fixed mobile layout

--- app.js
+++ app.js
@@ -33,15 +33,20 @@ function copyToClipboard() {
 var scene = document.getElementById("scene");
 var parallaxInstance = new Parallax(scene);
 
-const xSVG = document.querySelector('.xSVG');
+const xIcon = document.querySelector('.xSVG');
 const altSVG = document.querySelector('.altSVG');
 const mobile_navv = document.querySelector('.mobile-navv');
 
 altSVG.addEventListener('click', () => {
-  console.log('loggin')
-  console.log(mobile_navv);
   mobile_navv.style.display = 'flex';
-})
-xSVG.addEventListener('click', () => {
-  mobile_navv.style.display = 'none'
-})
+  altSVG.style.display = 'none';
+});
+
+xIcon.addEventListener('click', () => {
+  mobile_navv.style.display = 'none';
+  altSVG.style.display = '';
+});
+
+function closeNav (){
+  mobile_navv.style.display = 'none';
+}
